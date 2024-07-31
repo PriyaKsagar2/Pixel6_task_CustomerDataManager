1262c4ba2c4e9b0dc9115651aaf6d8b066197026
Priya Kishor Kshirsagar <2019bec134@sggs.ac.in>
2024-07-31 18:16:36 +0530

    Added some more functionalitied

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -15,11 +15,19 @@ function CustomerForm() {
 
   // Handle PAN input change
   const handlePanChange = (e) => {
-    setPan(e.target.value);
+    const panInput = e.target.value.toUpperCase();
+    setPan(panInput);
   };
 
   // Handle verifying PAN
   const handleVerifyClick = async () => {
+    const panPattern = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+    if (!panPattern.test(pan)) {
+      alert('Invalid PAN format');
+      setIsPanValid(false);
+      return;
+    }
+
     try {
       const response = await axios.post('https://lab.pixel6.co/api/verify-pan.php', { panNumber: pan });
       if (response.data.isValid) {
@@ -35,7 +43,18 @@ function CustomerForm() {
       setIsPanValid(false);
       alert('PAN not verified');
     }
-  };  
+  };
+
+  // Handle email input change
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  // Handle mobile input change
+  const handleMobileChange = (e) => {
+    const mobileInput = e.target.value.replace(/\D/g, '');
+    setMobile(mobileInput);
+  };
 
   // Handle adding a new address
   const addAddress = () => {
@@ -46,6 +65,16 @@ function CustomerForm() {
     }
   };
 
+  // Handle removing the most recently added address
+  const removeAddress = () => {
+    if (addresses.length > 1) {
+      const newAddresses = addresses.slice(0, -1);
+      setAddresses(newAddresses);
+    } else {
+      alert('You must have at least one address.');
+    }
+  };
+
   // Handle address input change
   const handleAddressChange = (index, field, value) => {
     const newAddresses = [...addresses];
@@ -53,25 +82,56 @@ function CustomerForm() {
     setAddresses(newAddresses);
   };
 
-  // Handle postcode change and fetch city/state
+// Handle postcode change and fetch city/state
   const handlePostcodeChange = async (index, value) => {
-    handleAddressChange(index, 'postcode', value);
-    try {
-      const response = await axios.post('https://lab.pixel6.co/api/get-postcode-details.php', { postcode: value });
-      const city = response.data.city[0].name;
-      const state = response.data.state[0].name;
-      handleAddressChange(index, 'city', city);
-      handleAddressChange(index, 'state', state);
-    } catch (error) {
-      console.error('Error fetching postcode details:', error);
+    if (value && (isNaN(value) || value <= 0 || value.length > 6)) {
+      alert("You can only enter upto 6 digits.");
+      return;
+    }
+
+    handleAddressChange(index, "postcode", value);
+    if (value.length <= 6) {
+      try {
+        const response = await axios.post(
+          "https://lab.pixel6.co/api/get-postcode-details.php",
+          { postcode: value }
+        );
+        const city = response.data.city[0].name;
+        const state = response.data.state[0].name;
+        handleAddressChange(index, "city", city);
+        handleAddressChange(index, "state", state);
+      } catch (error) {
+        console.error("Error fetching postcode details:", error);
+      }
     }
   };
 
+
   // Handle form submission
   const handleSubmit = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mobilePattern = /^[0-9]{10}$/;
+
     if (!isPanValid) {
       alert('Please verify the PAN before submitting.');
       return;
+    }
+
+    if (!emailPattern.test(email)) {
+      alert('Invalid email format.');
+      return;
+    }
+
+    if (!mobilePattern.test(mobile)) {
+      alert('Invalid mobile number format.');
+      return;
+    }
+
+    for (let address of addresses) {
+      if (!address.line1) {
+        alert('Address line 1 is required for all addresses.');
+        return;
+      }
     }
 
     const customer = { pan, fullName, email, mobile, addresses };
@@ -93,46 +153,45 @@ function CustomerForm() {
       </section>
 
       <main className="main">
-
         {!showCustomerList ? (
           <>
             <div className="pan">
               <h2>PAN Number</h2>
-              <h4>Enter valid PAN:</h4>
+              <h4>Enter valid PAN:<pre> *  (Here is correct format for your referance:"AAFNZ2078H")</pre> </h4>
               <input
                 type="text"
                 id="panNo"
                 value={pan}
                 onChange={handlePanChange}
                 placeholder="Enter valid PAN number"
+                maxLength="10"
               />
               <button id="verifyPan" onClick={handleVerifyClick}>Verify</button>
             </div>
             <div className="personal">
               <h2>Personal Details</h2>
               <div className="personalDetails">
-                <h4>Full name:</h4>
+                <h4>Full name:<pre> *</pre></h4>
                 <input type="text" id="name" value={fullName} placeholder="Your name according to PAN id" readOnly />
-                <h4>Email id:</h4>
+                <h4>Email id:<pre> *</pre></h4>
                 <input
                   type="email"
                   id="email"
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={handleEmailChange}
                   placeholder="Enter email id"
                   required
                   maxLength="255"
                 />
-                <h4>Mobile No:</h4>
+                <h4>Mobile No:<pre> *</pre></h4>
                 <input
                   type="tel"
                   id="mobile"
                   value={mobile}
-                  onChange={(e) => setMobile(e.target.value)}
-                  pattern="\+91[0-9]{10}"
+                  onChange={handleMobileChange}
                   required
-                  maxLength="13"
-                  placeholder="+91xxxxxxxxxx"
+                  maxLength="10"
+                  placeholder="xxxxxxxxxx"
                 />
               </div>
             </div>
@@ -140,14 +199,13 @@ function CustomerForm() {
               {addresses.map((address, index) => (
                 <div className="address" key={index}>
                   <h2>Address {index + 1}</h2>
-                  <h4>Address line 1:</h4>
+                  <h4>Address line 1: <pre> *</pre></h4>
                   <input
                     type="text"
                     id="address"
                     value={address.line1}
                     onChange={(e) => handleAddressChange(index, 'line1', e.target.value)}
                     required
-                    maxLength="700"
                   />
                   <h4>Address line 2:</h4>
                   <input
@@ -155,9 +213,8 @@ function CustomerForm() {
                     id="address"
                     value={address.line2}
                     onChange={(e) => handleAddressChange(index, 'line2', e.target.value)}
-                    maxLength="700"
                   />
-                  <h4>Post Code:</h4>
+                  <h4>Post Code: <pre> *</pre></h4>
                   <input
                     type="number"
                     value={address.postcode}
@@ -174,6 +231,7 @@ function CustomerForm() {
                 </div>
               ))}
               <button id="addressbutton" onClick={addAddress}>Add Address</button>
+              <button id="removeAddress" onClick={removeAddress}>Remove Address</button>
             </div>
           </>
         ) : (
--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -2,173 +2,254 @@ import React, { useState } from 'react';
 import './CustomerList.css';
 import axios from 'axios';
 
-//this component performs the editing and deleting operations on the customer data
+// This component performs the editing and deleting operations on the customer data
 function CustomerList({ customers, setCustomers }) {
+  // State variables to track editing state and edited customer data
   const [editingIndex, setEditingIndex] = useState(-1);
   const [editedCustomer, setEditedCustomer] = useState(null);
 
+  //handleEdit function handles the edit button click for a customer
   const handleEdit = (index) => {
+    // Set the editing index to the clicked customer's index
     setEditingIndex(index);
+    // Create a copy of the customer data at the clicked index for editing
     setEditedCustomer({ ...customers[index] });
   };
 
+  // Function to handle the delete button click for a customer
   const handleDelete = (index) => {
+    // Create a copy of the customer data array
     const newCustomers = [...customers];
+    // Remove the customer at the clicked index from the copy
     newCustomers.splice(index, 1);
+    // Update the state with the modified customer data
     setCustomers(newCustomers);
+    // Update localStorage with the modified customer data
     localStorage.setItem('customerList', JSON.stringify(newCustomers));
   };
 
-  const handleSave = () => {
+  // Function to handle the save button click after editing a customer
+  const handleSave = (index) => {
+    // Regular expressions for validating PAN, email, and mobile formats
+    const panPattern = /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mobilePattern = /^[0-9]{10}$/;
+
+    // Validate PAN format
+    if (!panPattern.test(editedCustomer.pan)) {
+      alert("Invalid PAN format");
+      return;
+    }
+
+    // Validate email format
+    if (!emailPattern.test(editedCustomer.email)) {
+      alert("Invalid email format");
+      return;
+    }
+
+    // Validate mobile number format
+    if (!mobilePattern.test(editedCustomer.mobile)) {
+      alert("Invalid mobile number format");
+      return;
+    }
+
+    // Loop through addresses and validate address line 1
+    for (let address of editedCustomer.addresses) {
+      if (!address.line1) {
+        alert("Address line 1 is required for all addresses.");
+        return;
+      }
+    }
+
+    // Create a copy of the customer data array
     const newCustomers = [...customers];
-    newCustomers[editingIndex] = editedCustomer;
+    // Update the customer data at the edited index with the edited data
+    newCustomers[index] = editedCustomer;
+    // Update the state with the modified customer data
     setCustomers(newCustomers);
+    // Update localStorage with the modified customer data
+    localStorage.setItem("customerList", JSON.stringify(newCustomers));
+    // Reset editing state variables
     setEditingIndex(-1);
     setEditedCustomer(null);
-    localStorage.setItem('customerList', JSON.stringify(newCustomers));
   };
 
+  // Function to handle the cancel button click while editing a customer
   const handleCancel = () => {
     setEditingIndex(-1);
     setEditedCustomer(null);
   };
 
-  const handleChange = (field, value) => {
-    setEditedCustomer({ ...editedCustomer, [field]: value });
-  };
-
-  const handleAddressChange = (addressIndex, field, value) => {
+  // Function to handle changes in address fields during editing
+  const handleAddressChange = (index, field, value) => {
     const newAddresses = [...editedCustomer.addresses];
-    newAddresses[addressIndex][field] = value;
+    newAddresses[index][field] = value;
     setEditedCustomer({ ...editedCustomer, addresses: newAddresses });
   };
 
+ // Function to handle postcode changes during editing and fetch city/state data
+  const handlePostcodeChange = async (index, value) => {
+    handleAddressChange(index, 'postcode', value);
+    try {
+      // Fetching API for getting city and state data based on postcode
+      const response = await axios.post('https://lab.pixel6.co/api/get-postcode-details.php', { postcode: value });
+      // Extract city and state data from the response
+      const city = response.data.city[0].name;
+      const state = response.data.state[0].name;
+      // Update address with fetched city and state data
+      handleAddressChange(index, 'city', city);
+      handleAddressChange(index, 'state', state);
+    } catch (error) {
+      console.error('Error fetching postcode details:', error);
+    }
+  };
+
+  // Function to add a new address
   const addAddress = () => {
     if (editedCustomer.addresses.length < 10) {
-      setEditedCustomer({
-        ...editedCustomer,
-        addresses: [...editedCustomer.addresses, { line1: '', line2: '', postcode: '', state: '', city: '' }]
-      });
+      const newAddresses = [...editedCustomer.addresses, { line1: '', line2: '', postcode: '', state: '', city: '' }];
+      setEditedCustomer({ ...editedCustomer, addresses: newAddresses });
     } else {
       alert('You can add a maximum of 10 addresses.');
     }
   };
 
-    // Handle postcode change and fetch city/state
-    const handlePostcodeChange = async (index, value) => {
-      handleAddressChange(index, 'postcode', value);
-      try {
-        const response = await axios.post('https://lab.pixel6.co/api/get-postcode-details.php', { postcode: value });
-        const city = response.data.city[0].name;
-        const state = response.data.state[0].name;
-        handleAddressChange(index, 'city', city);
-        handleAddressChange(index, 'state', state);
-      } catch (error) {
-        console.error('Error fetching postcode details:', error);
-      }
-    };
+  // Function to remove the most recently added address
+  const removeAddress = () => {
+    if (editedCustomer.addresses.length > 1) {
+      const newAddresses = editedCustomer.addresses.slice(0, -1);
+      setEditedCustomer({ ...editedCustomer, addresses: newAddresses });
+    } else {
+      alert('You must have at least one address.');
+    }
+  };
 
   return (
-    <div className="customer-list">
+    <div className='table'>
       <h2>Customer List</h2>
-      {customers.map((customer, index) => (
-        <div key={index} className="customerInfo">
-          <h4>Customer {index + 1}</h4>
-          {editingIndex === index ? (
-            <>
-              <input
-                type="text"
-                className='edit'
-                value={editedCustomer.pan}
-                placeholder="Enter valid PAN number"
-                // onChange={(e) => handleChange('pan', e.target.value)}
-                readOnly
-              />
-              <input
-                type="text"
-                className='edit'
-                value={editedCustomer.fullName}
-                placeholder="Enter your full name"
-                // onChange={(e) => handleChange('fullName', e.target.value)}
-              />
-              <input
-                type="email"
-                className='edit'
-                placeholder="Enter email id"
-                value={editedCustomer.email}
-                onChange={(e) => handleChange('email', e.target.value)}
-              />
-              <input
-                type="tel"
-                className='edit'
-                placeholder="+91xxxxxxxxxx"
-                value={editedCustomer.mobile}
-                onChange={(e) => handleChange('mobile', e.target.value)}
-              />
-              {editedCustomer.addresses.map((address, addressIndex) => (
-                <div key={addressIndex} className="edit-address">
-                  <h4>Address {addressIndex + 1}</h4>
-                  <input
-                    type="text"
-                    className='edit'
-                    value={address.line1}
-                    onChange={(e) => handleAddressChange(addressIndex, 'line1', e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    className='edit'
-                    value={address.line2}
-                    onChange={(e) => handleAddressChange(addressIndex, 'line2', e.target.value)}
-                  />
-                  <input
-                    type="number"
-                    className='edit'
-                    placeholder="Select valid Post Code"
-                    value={address.postcode}
-                    onChange={(e) => handlePostcodeChange(index, e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    className='edit'
-                    placeholder="State"
-                    value={address.state}
-                    readOnly
-                  />
-                  <input
-                    type="text"
-                    className='edit'
-                    placeholder="City"
-                    value={address.city}
-                    readOnly
-                  />
-                </div>
-              ))}
-              <button id='addressbutton2' onClick={addAddress}>Add Address</button>
-              <button id='save' onClick={handleSave}>Save</button>
-              <button id='cancel' onClick={handleCancel}>Cancel</button>
-            </>
-          ) : (
-            <>
-              <p>PAN: {customer.pan}</p>
-              <p>Full Name: {customer.fullName}</p>
-              <p>Email: {customer.email}</p>
-              <p>Mobile: {customer.mobile}</p>
-              {customer.addresses.map((address, addressIndex) => (
-                <div key={addressIndex} className="view-address">
-                  <p>Address {addressIndex + 1}</p>
-                  <p>Line 1: {address.line1}</p>
-                  <p>Line 2: {address.line2}</p>
-                  <p>Post Code: {address.postcode}</p>
-                  <p>State: {address.state}</p>
-                  <p>City: {address.city}</p>
-                </div>
-              ))}
-              <button id='edit' onClick={() => handleEdit(index)}>Edit</button>
-              <button id='delete' onClick={() => handleDelete(index)}>Delete</button>
-            </>
-          )}
-        </div>
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>PAN</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Addresses</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer, index) => (
+            <tr key={index}>
+              {editingIndex === index ? (
+                <>
+                  <td>
+                    <input
+                      type="text"
+                      className='edit'
+                      value={editedCustomer.pan}
+                      maxLength="10"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      className='edit'
+                      value={editedCustomer.fullName}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="email"
+                      className='edit'
+                      value={editedCustomer.email}
+                      onChange={(e) => setEditedCustomer({ ...editedCustomer, email: e.target.value })}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="tel"
+                      className='edit'
+                      value={editedCustomer.mobile}
+                      onChange={(e) => setEditedCustomer({ ...editedCustomer, mobile: e.target.value })}
+                      maxLength="10"
+                    />
+                  </td>
+                  <td>
+                    {editedCustomer.addresses.map((address, addrIndex) => (
+                      <div key={addrIndex}>
+                      <h4 style={{ color: '#03045e', backgroundColor: '#caf0f8', margin: '8px'}}>Address:</h4>
+                        <input
+                          type="text"
+                          className='editAdd'
+                          value={address.line1}
+                          onChange={(e) => handleAddressChange(addrIndex, 'line1', e.target.value)}
+                          placeholder="Address line 1"
+                          required
+                        />
+                        <input
+                          type="text"
+                          className='editAdd'
+                          value={address.line2}
+                          onChange={(e) => handleAddressChange(addrIndex, 'line2', e.target.value)}
+                          placeholder="Address line 2"
+                        />
+                        <input
+                          type="number"
+                          className='editAdd'
+                          value={address.postcode}
+                          onChange={(e) => handlePostcodeChange(addrIndex, e.target.value)}
+                          placeholder="Postcode"
+                          maxLength="6"
+                        />
+                        <input
+                          type="text"
+                          className='editAdd'
+                          value={address.state}
+                          placeholder="State"
+                          readOnly
+                        />
+                        <input
+                          type="text"
+                          className='editAdd'
+                          value={address.city}
+                          placeholder="City"
+                          readOnly
+                        />
+                      </div>
+                    ))}
+                    <button id='editAdd' onClick={addAddress}>Add Address</button>
+                    <button id='editAdd' onClick={removeAddress}>Remove Address</button>
+                  </td>
+                  <td>
+                    <button id='save' onClick={() => handleSave(index)}>Save</button>
+                    <button id='cancel' onClick={handleCancel}>Cancel</button>
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td>{customer.pan}</td>
+                  <td>{customer.fullName}</td>
+                  <td>{customer.email}</td>
+                  <td>{customer.mobile}</td>
+                  <td>
+                    {customer.addresses.map((address, addrIndex) => (
+                      <div key={addrIndex}>
+                      <h4 style={{ color: '#03045e', backgroundColor: '#caf0f8', margin: '8px'}}>Address:</h4>
+                        {address.line1}, {address.line2}, {address.postcode}, {address.city}, {address.state}
+                      </div>
+                    ))}
+                  </td>
+                  <td>
+                    <button id='edit' onClick={() => handleEdit(index)}>Edit</button>
+                    <button id='delete' onClick={() => handleDelete(index)}>Delete</button>
+                  </td>
+                </>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
